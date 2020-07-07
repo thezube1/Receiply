be8f2f409edb9f35be049db499265717baf4ab1f
Zubin XPS <zubin.hydrie@hotmail.com>
2020-07-07 14:01:03 -0700

    Added signup states

--- frontend/src/components/signup/signupform.jsx
+++ frontend/src/components/signup/signupform.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 import "./signup.css";
 
 class SignupForm extends Component {
@@ -11,11 +11,12 @@ class SignupForm extends Component {
     pass: "",
   };
 
-  handleEmail = (event) => {
-    this.setState({ email: event.target.value });
+  handleChange = (inputType) => (event) => {
+    this.setState({ [inputType]: event.target.value });
   };
 
   render() {
+    console.log(this.state.email, this.state.first);
     return (
       <div>
         <div id="formContainer">
@@ -25,16 +26,36 @@ class SignupForm extends Component {
             type="text"
             className="input"
             value={this.state.email}
-            onChange={this.handleEmail}
+            onChange={this.handleChange("email")}
           />
           <div className="description">First name</div>
-          <input type="text" className="input" />
+          <input
+            type="text"
+            className="input"
+            value={this.state.first}
+            onChange={this.handleChange("first")}
+          />
           <div className="description">Last name</div>
-          <input type="text" className="input" />
+          <input
+            type="text"
+            className="input"
+            value={this.state.last}
+            onChange={this.handleChange("last")}
+          />
           <div className="description">Username</div>
-          <input type="text" className="input" />
+          <input
+            type="text"
+            className="input"
+            value={this.state.user}
+            onChange={this.handleChange("user")}
+          />
           <div className="description">Password</div>
-          <input type="text" className="input" />
+          <input
+            type="text"
+            className="input"
+            value={this.state.pass}
+            onChange={this.handleChange("pass")}
+          />
           <br />
           <input type="button" value="Submit" id="submitButton" />
         </div>
