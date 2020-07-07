9535e5557f5bb3ada48469e7ba308fe6ed6b9b12
Zubin <zubin.hydrie@hotmail.com>
2020-07-07 12:50:07 -0700

    Adding event handling

--- frontend/src/components/signup/signupform.jsx
+++ frontend/src/components/signup/signupform.jsx
@@ -10,21 +10,31 @@ class SignupForm extends Component {
     user: "",
     pass: "",
   };
+
+  handleEmail = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
   render() {
     return (
       <div>
         <div id="formContainer">
           <div id="header">Enter your information to sign up</div>
-          <div class="description">Email</div>
-          <input type="text" class="input" />
-          <div class="description">First name</div>
-          <input type="text" class="input" />
-          <div class="description">Last name</div>
-          <input type="text" class="input" />
-          <div class="description">Username</div>
-          <input type="text" class="input" />
-          <div class="description">Password</div>
-          <input type="text" class="input" />
+          <div className="description">Email</div>
+          <input
+            type="text"
+            className="input"
+            value={this.state.email}
+            onChange={this.handleEmail}
+          />
+          <div className="description">First name</div>
+          <input type="text" className="input" />
+          <div className="description">Last name</div>
+          <input type="text" className="input" />
+          <div className="description">Username</div>
+          <input type="text" className="input" />
+          <div className="description">Password</div>
+          <input type="text" className="input" />
           <br />
           <input type="button" value="Submit" id="submitButton" />
         </div>
