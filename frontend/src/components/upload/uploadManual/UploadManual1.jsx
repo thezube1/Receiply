ba5d3e34475eea7b851a7524829c117adda12e7c
Zubin <zubin.hydrie@hotmail.com>
2020-07-17 21:52:30 -0700

    Added more stuff

--- frontend/src/components/upload/uploadManual/UploadManual1.jsx
+++ frontend/src/components/upload/uploadManual/UploadManual1.jsx
@@ -9,16 +9,42 @@ class UploadManual1 extends Component {
     localStorage.setItem(inputType, event.target.value);
     this.setState({ [inputType]: event.target.value });
   };
+
+  getChanges = (inputType) => {
+    return localStorage.getItem(inputType);
+  };
   render() {
     return (
-      <div>
-        <form autocomplete="on">
-          <input
+      <div id="uploadManual1Wrapper">
+        <div>
+          <div className="uploadManualHeader">Name of Recipe:</div>
+          <textarea
+            id="uploadManualRecipeName"
+            className="uploadManualRecipeInput"
             type="text"
-            value={this.state.input}
-            onChange={this.handleChange("input")}
+            defaultValue={this.getChanges("recipeName")}
+            onChange={this.handleChange("recipeName")}
           />
-        </form>
+        </div>
+        <div>
+          <div className="uploadManualHeader">Creator(s):</div>
+          <textarea
+            className="uploadManualRecipeInput"
+            type="text"
+            defaultValue={this.getChanges("recipeCreator")}
+            onChange={this.handleChange("recipeCreator")}
+          />
+        </div>
+        <div>
+          <div className="uploadManualHeader">Description:</div>
+          <textarea
+            className="uploadManualRecipeInput"
+            id="uploadManualRecipeDescription"
+            type="text"
+            defaultValue={this.getChanges("recipeDescription")}
+            onChange={this.handleChange("recipeDescription")}
+          />
+        </div>
       </div>
     );
   }
