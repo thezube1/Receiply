2be0c0e3746eefbceea8bbf1a361fd49db819e7c
Zubin <zubin.hydrie@hotmail.com>
2020-08-15 18:48:59 -0700

    Added basic OCR functionality

--- frontend/src/components/upload/uploadPhoto/UploadPhoto.jsx
+++ frontend/src/components/upload/uploadPhoto/UploadPhoto.jsx
@@ -1,9 +1,55 @@
 import React, { Component } from "react";
+import "./UploadPhoto.css";
+import Tesseract from "tesseract.js";
 
 class UploadPhoto extends Component {
-  state = {};
+  state = {
+    selectedFile: undefined,
+    name: "",
+  };
+
+  onFileChange = (event) => {
+    this.setState({ selectedFile: event.target.files[0] });
+  };
+
+  handlePhotoName = () => {
+    if (this.state.selectedFile !== undefined) {
+      return this.state.selectedFile.name;
+    } else {
+      return "Choose a file";
+    }
+  };
+
+  handleProcess = (photo) => {
+    Tesseract.recognize(photo, "eng", { logger: (m) => console.log(m) }).then(
+      ({ data: { text } }) => {
+        console.log(text);
+      }
+    );
+  };
   render() {
-    return <div>Select photo</div>;
+    return (
+      <div id="uploadPhotoWrapper">
+        <div id="uploadPhotoContent">
+          <div>Upload Photo:</div>
+          <input
+            id="uploadManualPhoto"
+            type="file"
+            name="uploadManualPhoto"
+            accept="image/x-png,image/gif,image/jpeg"
+            onChange={this.onFileChange}
+          />
+          <label htmlFor="uploadManualPhoto">{this.handlePhotoName()}</label>
+          <div>
+            <input
+              type="button"
+              value="Submit"
+              onClick={() => this.handleProcess(this.state.selectedFile)}
+            />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
